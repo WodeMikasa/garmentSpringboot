c3f923fef2eab716e83214767baea5c6ebb6e576
pj <1626615930@qq.com>
2022-05-30 11:10:54 +0800

    初始化

---
+++ garmentspringboot/src/main/java/cn/ccsu/GarmentSpringbootApplication.java
@@ -0,0 +1,11 @@
+package cn.ccsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarmentSpringbootApplication {
+    public static void main(String[] args){
+        SpringApplication.run(GarmentSpringbootApplication.class,args);
+    }
+}
